2a1bfc394049b34693f7f9fb8edec48c25f29c7e
Wen Jiazhi <jiazhi.wen@pingcap.com>
2023-09-06 17:44:52 +0800

    fix

--- test100/generate.py
+++ test100/generate.py
@@ -1,31 +1,6 @@
 import copy
 import json
 
-# endpoint =  {
-#     "name": "/test",
-#     "description": "",
-#     "method": "GET",
-#     "endpoint": "/testf",
-#     "data_source": {
-#       "cluster_id": 3778994
-#     },
-#     "params": [
-#       {
-#         "name": "id",
-#         "type": "integer",
-#         "required": 1,
-#         "default": "",
-#         "description": ""
-#       }
-#     ],
-#     "settings": {
-#       "timeout": 5000,
-#       "row_limit": 50
-#     },
-#     "sql_file": "sql/GET-testf.sql",
-#     "type": "sql_endpoint",
-#     "return_type": "json"
-#   }
 endpoint = {
     "name": "/test",
     "description": "",
